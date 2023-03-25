d92cbf7d61e8ce2ee3a30436380479ff8636f63d
kabita bhandari <36861110+kabitabhandari@users.noreply.github.com>
2023-03-25 18:29:31 -0500

    state in react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { useState } from 'react';
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <h1>Counters that update separately</h1>
+        <MyButton />
+        <MyButton />
+      </div>
   );
 }
 
-export default App;
+function MyButton() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return (
+      <button onClick={handleClick}>
+        Clicked {count} times
+      </button>
+  );
+}
